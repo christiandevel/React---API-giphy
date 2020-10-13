8dcab21325663ce5b26f51c39910ab279f0704f1
ChristianCeballos <christianmoreno531@gmail.com>
2020-10-13 17:07:25 -0500

    Docs agregado

---
+++ docs/precache-manifest.3f7d23ae772146e27469be3cc504e248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96fca9107e4cd34d88a3734e07ff6eaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb8ca7b2d9982e91f498",
+    "url": "/static/css/main.a52f80b7.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb8ca7b2d9982e91f498",
+    "url": "/static/js/main.ef1c79a1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
